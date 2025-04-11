25bd1f8a0923ba367f75df4ba7cd4377b37b3575
Patolll <patryk_dmochowski@wp.pl>
2025-04-11 18:46:55 +0200

    fetching data and styling

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const line = document.querySelector(".line");
+const direction = document.querySelector(".direction");
+const departure = document.querySelector(".departure");
+const time = document.getElementById("time");
 fetch("https://www.zditm.szczecin.pl/api/v1/displays/97311")
   .then((response) => {
     if (!response.ok) {
@@ -6,23 +10,35 @@ fetch("https://www.zditm.szczecin.pl/api/v1/displays/97311")
     return response.json();
   })
   .then((data) => {
-    console.log(data.departures);
+    if (!data.departures) {
+      console.error("No departures found in the response data.");
+      return;
+    }
+    line.innerHTML = ""; // Clear previous content
+    direction.innerHTML = ""; // Clear previous content
+    departure.innerHTML = ""; // Clear previous content
+
+    data.departures.forEach((element) => {
+      const lineElement = document.createElement("li");
+      const directionElement = document.createElement("li");
+      const departureElement = document.createElement("li");
+      console.log(element);
+      lineElement.innerText = element.line_number;
+      line.appendChild(lineElement);
+      directionElement.innerText = element.direction;
+      direction.appendChild(directionElement);
+      departureElement.innerText = element.time_scheduled;
+      departure.appendChild(departureElement);
+    });
   })
   .catch((error) => {
     console.error("There was a problem with the fetch operation:", error);
   });
-// fetch(" https://www.zditm.szczecin.pl/api/v1/stops")
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok " + response.statusText);
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     data.data.forEach((element) => {
-//       console.log(element);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("There was a problem with the fetch operation:", error);
-//   });
+function updateTime() {
+  const today = new Date();
+  const hours = String(today.getHours()).padStart(2, "0");
+  const minutes = String(today.getMinutes()).padStart(2, "0");
+  time.innerText = `${hours}:${minutes}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
